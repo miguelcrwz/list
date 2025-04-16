c197293f9567f404057c2302e566f5084dc3c3b1
Miguel <miguwlcrwz@gmail.com>
2025-04-15 22:03:45 -0300

    :tada:

---
+++ src/main/java/com/miguelcrwz/list/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.miguelcrwz.list.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository <UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
